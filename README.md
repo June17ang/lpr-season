20d5af8bdd0dfee43691ab6dd17e53424d1b8259
June Ang <June Ang>
2020-09-10 15:53:13 +0800

    Implement season project using react

---
+++ README.md
@@ -0,0 +1,4 @@
+## Season
+
+Learn and practice react from udemy course
+[Modern React with Redux](https://www.udemy.com/course/react-redux)
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./components/CommentDetail";
+import ApprovalCard from "./components/ApprovalCard";
+
+const App = () => {
+  return <div>Season</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
